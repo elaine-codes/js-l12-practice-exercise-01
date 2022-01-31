49cdd8906dc97cdc5d01ad839f7fa2d7cffd7269
elaine_alberto <elainecodes@gmail.com>
2022-01-31 12:52:51 -0500

    Added missing variable
    Corrected API parameter and added code for event listener line 31

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function() {
-    const userRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function(numUsers) {
+    const userRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await userRequest.json();
 
     const userResults = data.results;
     displayUser(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
@@ -26,3 +27,8 @@ const displayUsers = function (userResults) {
     randomFolks.append(userDiv);
     }
 };
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+  });
